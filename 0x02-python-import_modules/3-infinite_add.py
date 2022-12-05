7dcac6c5747503a64ec788b633040fac2aeac547
RamouSgh <ramousazawornu@gmail.com>
2022-12-04 17:50:49 -0800

    Task 3 review

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-
 import sys
 
-if __name__ == "__main__":
-
-    agv = sys.argv
+if __name__ == '__main__':
+    av = sys.argv
+    l_av = len(av)
     sum = 0
 
-    if len(agv) > 1:
-        for i in range(1, len(agv)):
+    if l_av > 1:
+        for i in range(1, l_av):
+            sum += int(av[i])
 
-             sum += int(agv[i])
-
-        print(sum)
+    print(sum)
