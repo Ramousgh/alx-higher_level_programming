5a5fa95b23a99b6d632ab5ad4943e124c5f7295d
RamouSgh <ramousazawornu@gmail.com>
2022-12-05 16:58:42 -0800

    Ramousgh

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 def multiple_returns(sentence):
     tup = tuple(sentence)
     if tup:
-        for i in tup:
-            return (len(tup), tup[0])
+        return (len(tup), tup[0])
     else:
         return ( 0, None)
